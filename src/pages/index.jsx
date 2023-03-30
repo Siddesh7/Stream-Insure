442332c34e00e7b83dde666acefd22ab2c93d9d0
Siddesh <siddeshsankhya@gmail.com>
2023-03-30 23:50:49 +0530

    final conflicts

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -29,6 +29,25 @@ export default function Home() {
                     <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg relative h-[211px]">
                         <div className="p-6">
                             <h2 className="text-lg font-bold text-white mb-2">
+                                Crypto Asset Insurance
+                            </h2>
+                            <p className="text-gray-400 text-base">
+                                Protect your crypto assets from high volatility
+                                by buying insurance.
+                            </p>
+                            <a
+                                href="/insurance/create"
+                                className="mt-4 bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded absolute bottom-[20px]"
+                            >
+                                Create Insurance
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-full sm:w-1/2 md:w-1/3">
+                    <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg relative h-[211px]">
+                        <div className="p-6">
+                            <h2 className="text-lg font-bold text-white mb-2">
                                 Weather Calamity Insurance
                             </h2>
                             <p className="text-gray-400 text-base">
--- src/pages/insurance/create.jsx
+++ src/pages/insurance/create.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useContractWrite, usePrepareContractWrite } from "wagmi";
+import Navbar from "../../components/Navbar";
 
 import { deployer } from "../../contract";
 
@@ -17,7 +18,7 @@ export default function CreateBond() {
 
         setInsurance((prevData) => ({
             ...prevData,
-            [name]: value,
+            [name]: value.toString(),
         }));
     };
     const { config: createInsuranceConfig } = usePrepareContractWrite({
@@ -37,156 +38,171 @@ export default function CreateBond() {
 
     return (
         <div>
-            <div className="card w-96 bg-neutral text-neutral-content">
-                <div className="card-body items-center text-center">
-                    <h2 className="card-title">Asset Insurance</h2>
-                    <p>Create an insurance bond tracking an asset listed.</p>
-                    <div className="card-actions justify-end">
-                        <label htmlFor="my-modal-3" className="btn btn-primary">
+            <Navbar />
+            <div className="w-[90%] m-auto mt-[100px]">
+                <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg relative h-[211px]">
+                    <div className="p-6">
+                        <h2 className="text-lg font-bold text-white mb-2">
+                            Crypto Asset Insurance
+                        </h2>
+                        <p className="text-gray-400 text-base">
+                            Create an insurance bond tracking an asset listed.
+                        </p>
+
+                        <label
+                            htmlFor="my-modal-3"
+                            className="mt-4 bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded absolute bottom-[20px]"
+                        >
                             Create
                         </label>
                     </div>
                 </div>
-            </div>
-            {/* The button to open modal */}
+                {/* The button to open modal */}
 
-            {/* Put this part before </body> tag */}
-            <input type="checkbox" id="my-modal-3" className="modal-toggle" />
-            <div className="modal">
-                <div className="modal-box relative">
-                    <label
-                        htmlFor="my-modal-3"
-                        className="btn btn-sm btn-circle absolute right-2 top-2"
-                    >
-                        ✕
-                    </label>
-                    <h3 className="text-lg font-bold">
-                        Please provide the details for your insurance bond
-                        creation
-                    </h3>
-                    <div className="flex flex-col gap-4 mt-[10px]">
-                        <select
-                            className="select select-primary w-full"
-                            onChange={handleInputChange}
-                            name="assetInsured"
+                {/* Put this part before </body> tag */}
+                <input
+                    type="checkbox"
+                    id="my-modal-3"
+                    className="modal-toggle"
+                />
+                <div className="modal">
+                    <div className="modal-box relative">
+                        <label
+                            htmlFor="my-modal-3"
+                            className="btn btn-sm btn-circle absolute right-2 top-2"
                         >
-                            <option disabled selected>
-                                What asset are you creating the insurance for?
-                            </option>
-                            <option
-                                value={
-                                    "0x007A22900a3B98143368Bd5906f8E17e9867581b"
-                                }
+                            ✕
+                        </label>
+                        <h3 className="text-lg font-bold">
+                            Please provide the details for your insurance bond
+                            creation
+                        </h3>
+                        <div className="flex flex-col gap-4 mt-[10px]">
+                            <select
+                                className="select select-primary w-full"
+                                onChange={handleInputChange}
+                                name="assetInsured"
                             >
-                                Bitcoin
-                            </option>
-                            <option
-                                value={
-                                    "0x0715A7794a1dc8e42615F059dD6e406A6594651A"
-                                }
+                                <option disabled selected>
+                                    What asset are you creating the insurance
+                                    for?
+                                </option>
+                                <option
+                                    value={
+                                        "0x007A22900a3B98143368Bd5906f8E17e9867581b"
+                                    }
+                                >
+                                    Bitcoin
+                                </option>
+                                <option
+                                    value={
+                                        "0x0715A7794a1dc8e42615F059dD6e406A6594651A"
+                                    }
+                                >
+                                    Ethereum
+                                </option>
+                                <option
+                                    value={
+                                        "0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada"
+                                    }
+                                >
+                                    Matic
+                                </option>
+                                <option
+                                    value={
+                                        "0xEB0fb293f368cE65595BeD03af3D3f27B7f0BD36"
+                                    }
+                                >
+                                    Solana
+                                </option>
+                            </select>
+                            <select
+                                className="select select-primary w-full"
+                                onChange={handleInputChange}
+                                name="assetDeposited"
+                                required
                             >
-                                Ethereum
-                            </option>
-                            <option
-                                value={
-                                    "0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada"
-                                }
-                            >
-                                Matic
-                            </option>
-                            <option
-                                value={
-                                    "0xEB0fb293f368cE65595BeD03af3D3f27B7f0BD36"
-                                }
-                            >
-                                Solana
-                            </option>
-                        </select>
-                        <select
-                            className="select select-primary w-full"
-                            onChange={handleInputChange}
-                            name="assetDeposited"
-                            required
-                        >
-                            <option disabled selected>
-                                Which asset are you depositing into the bond?
-                            </option>
+                                <option disabled selected>
+                                    Which asset are you depositing into the
+                                    bond?
+                                </option>
 
-                            <option
-                                value={
-                                    "0x001b3b4d0f3714ca98ba10f6042daebf0b1b7b6f"
-                                }
+                                <option
+                                    value={
+                                        "0x001b3b4d0f3714ca98ba10f6042daebf0b1b7b6f"
+                                    }
+                                >
+                                    DAI
+                                </option>
+                                <option
+                                    value={
+                                        "0x1abd0a92ce2913a50c8a82c90f30736e50770e90"
+                                    }
+                                >
+                                    fUSD
+                                </option>
+                            </select>
+                            <input
+                                type="number"
+                                placeholder="How much are you depositing?"
+                                className="input input-bordered input-info w-full"
+                                onChange={handleInputChange}
+                                name="depositAmount"
+                            />
+                            <select
+                                className="select select-primary w-full"
+                                onChange={handleInputChange}
+                                name="premiumAsset"
                             >
-                                DAI
-                            </option>
-                            <option
-                                value={
-                                    "0x1abd0a92ce2913a50c8a82c90f30736e50770e90"
-                                }
-                            >
-                                fUSD
-                            </option>
-                        </select>
-                        <input
-                            type="number"
-                            placeholder="How much are you depositing?"
-                            className="input input-bordered input-info w-full"
-                            onChange={handleInputChange}
-                            name="depositAmount"
-                        />
-                        <select
-                            className="select select-primary w-full"
-                            onChange={handleInputChange}
-                            name="premiumAsset"
-                        >
-                            <option disabled selected>
-                                Which asset will you accept for premium
-                                payments?
-                            </option>
-                            <option
-                                value={
-                                    "0x5D8B4C2554aeB7e86F387B4d6c00Ac33499Ed01f"
-                                }
-                            >
-                                fDAIx
-                            </option>
-                            <option
-                                value={
-                                    "0x918E0d5C96cAC79674E2D38066651212be3C9C48"
-                                }
-                            >
-                                fTUSDx
-                            </option>
-                            <option
-                                value={
-                                    "0x42bb40bF79730451B11f6De1CbA222F17b87Afd7"
-                                }
-                            >
-                                fUSDCx
-                            </option>
-                        </select>
-                        <input
-                            type="number"
-                            placeholder="How much is the monthly premium? 100/mo"
-                            className="input input-bordered input-info w-full"
-                            onChange={handleInputChange}
-                            name="monthlyPremium"
-                        />
-                        <input
-                            type="number"
-                            placeholder="Threshold price? Triggers payment to buyer(insured amount)"
-                            className="input input-bordered input-info w-full"
-                            required
-                            onChange={handleInputChange}
-                            name="thresholdPrice"
-                        />
+                                <option disabled selected>
+                                    Which asset will you accept for premium
+                                    payments?
+                                </option>
+                                <option
+                                    value={
+                                        "0x5D8B4C2554aeB7e86F387B4d6c00Ac33499Ed01f"
+                                    }
+                                >
+                                    fDAIx
+                                </option>
+                                <option
+                                    value={
+                                        "0x918E0d5C96cAC79674E2D38066651212be3C9C48"
+                                    }
+                                >
+                                    fTUSDx
+                                </option>
+                                <option
+                                    value={
+                                        "0x42bb40bF79730451B11f6De1CbA222F17b87Afd7"
+                                    }
+                                >
+                                    fUSDCx
+                                </option>
+                            </select>
+                            <input
+                                type="number"
+                                placeholder="How much is the monthly premium? 100/mo"
+                                className="input input-bordered input-info w-full"
+                                onChange={handleInputChange}
+                                name="monthlyPremium"
+                            />
+                            <input
+                                type="number"
+                                placeholder="Threshold price? Triggers payment to buyer(insured amount)"
+                                className="input input-bordered input-info w-full"
+                                required
+                                onChange={handleInputChange}
+                                name="thresholdPrice"
+                            />
 
-                        <button
-                            className="btn btn-success"
-                            onClick={createInsurance}
-                        >
-                            Create bond
-                        </button>
+                            <button
+                                className="btn btn-success"
+                                onClick={createInsurance}
+                            >
+                                Create bond
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
